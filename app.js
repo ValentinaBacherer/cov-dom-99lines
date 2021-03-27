f627e8b223dd52519183f64aff951310c7e5c9ba
Valentina Bacherer <valentina.bacherer@gmail.com>
2021-03-27 20:14:37 -0300

    initial

---
+++ app.js
@@ -0,0 +1,7 @@
+const friends = ['Joe', 'Clara', 'Ana', 'Ella', 'Andrea']
+
+for (friend of friends){
+    console.log(`${friend}`);
+    for(let i=0 ; i<100 ; i++)
+console.log(`${99-i}lines of code in the file,${99-i}lines of code; ${friend} strikes one out`);
+}
