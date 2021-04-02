87e22bce3d1d52b4469caf7d1b11140c4137a139
Valentina Bacherer <valentina.bacherer@gmail.com>
2021-04-02 14:00:54 -0300

    part 1 done

--- app.js
+++ app.js
@@ -1,7 +1,13 @@
 const friends = ['Joe', 'Clara', 'Ana', 'Ella', 'Andrea']
 
-for (friend of friends){
+for (friend of friends) {
     console.log(`${friend}`);
-    for(let i=0 ; i<100 ; i++)
-console.log(`${99-i}lines of code in the file,${99-i}lines of code; ${friend} strikes one out`);
+    for (let i = 0; i < 99; i++) {
+        if (i == 98) {
+
+            console.log(`${99 - i} line of code in the file, ${99 - i} line of code; ${friend} strikes one out, clears it all out, no more lines of code in the file.`);
+        } else {
+            console.log(`${99 - i}lines of code in the file,${99 - i}lines of code; ${friend} strikes one out, clears it all out, `);
+        }
+    }
 }
